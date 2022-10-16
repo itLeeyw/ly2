91e04bc0bac3e17deea81a49a92970af88cc7257
leeyw <1764254407@qq.com>
2022-10-17 00:17:17 +0800

    feat: init ly2

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: {
+    "extends": [
+      "plugin:@typescript-eslint/recommended",
+      // 用于关闭 ESLint 相关的格式规则集，具体可查看 https://github.com/prettier/eslint-config-prettier/blob/master/index.js
+      "prettier",
+      // 用于关闭 @typescript-eslint/eslint-plugin 插件相关的格式规则集，具体可查看 https://github.com/prettier/eslint-config-prettier/blob/master/%40typescript-eslint.js
+      "prettier/@typescript-eslint",
+    ]
+  }
+};
---
+++ types/index.d.ts
@@ -0,0 +1,1 @@
+declare const n = 1;
